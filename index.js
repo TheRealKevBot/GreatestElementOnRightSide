04bf691b00de755b500ee47871cb6f07a5c578fd
TheRealKevBot <Kglas001@gmail.com>
2021-06-23 17:04:37 -0600

    Build

--- index.js
+++ index.js
@@ -3,9 +3,22 @@
 
 // After doing so, return the array. 
 
-function replaceElements(arr) {
-    
-}; 
+function replaceElements(arr){
+    let newArr = []
+    let curArr = [...arr]
+    arr.forEach(curNum => {
+        curArr.shift()
+        bigNum = 0 
+        curArr.forEach(num => {
+            num > bigNum ? bigNum = num : null
+        })
+        bigNum === 0 ? bigNum = -1 : null
+        newArr.push(bigNum)
+    })
+    return newArr
+}; // Long Run Time, need to refactor....
 
 console.log(replaceElements([17,18,5,4,6,1]))
-console.log(replaceElements([400]))
+console.log(replaceElements([400, 100, 500, 600, 200, 100, 10]))
+console.log(replaceElements([10, 20 , 400, 600, 500, 300, 100]))
+console.log(replaceElements([400, 900, 1000]))
