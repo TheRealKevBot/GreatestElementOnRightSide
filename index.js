db9ed8e092c8be7dce6bfe55d4214c5362d27998
TheRealKevBot <Kglas001@gmail.com>
2021-06-23 16:09:09 -0600

    First

---
+++ index.js
@@ -0,0 +1,11 @@
+// Given an array arr, replace every element in that array with the greatest element among the elements to its right, 
+// and replace the last element with -1.
+
+// After doing so, return the array. 
+
+function replaceElements(arr) {
+    
+}; 
+
+console.log(replaceElements([17,18,5,4,6,1]))
+console.log(replaceElements([400]))
